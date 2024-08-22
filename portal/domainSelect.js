74b51c24dcb26e5dc5ec1ad45efafa0908991e85
Dylan Howard <dylanhoward6@gamil.com>
2024-08-22 15:41:57 -0500

    Implemented fixes for event listener

--- portal/domainSelect.js
+++ portal/domainSelect.js
@@ -6,21 +6,42 @@
  * Author: Dylan Howard
 */
 
-/* Edit these for your environment */
-const selectOptions = [
-  '@warren.kyschools.us',
-  '@stu.warren.kyschools.us',
-];
+function addAuthButtonHandler() {
+  const authnButton = document.getElementById('authn-go-button');
 
-/* Functions -- Edit with care */
+  if (authnButton) {
+    authnButton.addEventListener("click", () => appendDomain());
+  } else {
+    setTimeout(addAuthButtonHandler, 100);
+  }
+}
+
+function appendDomain() {
+  const usernameInput = document.getElementById('identification');
+  const domainInput = document.getElementById('domain-select');
+
+  if (usernameInput && domainInput) {
+    if (domainInput.value != '') {
+      /* Find the end index of the username */
+      const atIndex = usernameInput.value.indexOf('@');
+      const usernameEndIndex = atIndex === -1 ? usernameInput.value.length : atIndex;
+      
+      /* Appends the domain to the username */
+      usernameInput.value = usernameInput.value.substring(0, usernameEndIndex) + domainInput.value;
+
+      /* Handles textbox update */
+      usernameInput.focus();
+      usernameInput.blur();
+    }
+  }
+}
+
 function createOption(option, value, selected) {
-
   /* Creates option element */
   const optionElement = document.createElement('option');
   optionElement.value = value;
   optionElement.disabled = value === '';
   optionElement.selected = selected;
-
   /* Sets option element text */
   const label = document.createTextNode(option);
   optionElement.appendChild(label);
@@ -32,6 +53,7 @@ function appendDomainSelect() {
   const usernameInput = document.getElementById('authn-username-input');
 
   if (usernameInput) {
+    /* Guards from repeat domain select insertions */
     const domainInput = document.getElementById('domain-select');
     if (domainInput) {
       console.log('Domain already inserted...');
@@ -45,12 +67,13 @@ function appendDomainSelect() {
     domainSelect.classList.add('cs-login-select');
 
     domainSelect.appendChild(createOption('Please select', '', true));
-    for (let i = 0; i < selectOptions.length; i += 1) {
-      domainSelect.appendChild(createOption(selectOptions[i], selectOptions[i], false));
-    }
+    domainSelect.appendChild(createOption('@warren.kyschools.us', '@warren.kyschools.us', false));
+    domainSelect.appendChild(createOption('@stu.warren.kyschools.us', '@stu.warren.kyschools.us', false));
 
     document.getElementById('authn-username-input')
       .appendChild(domainSelect);
+
+    addAuthButtonHandler();
 
     setTimeout(appendDomainSelect, 2000);
   } else {
@@ -58,28 +81,7 @@ function appendDomainSelect() {
   }
 }
 
-function updateRequestHandler() {
-  const httpSend = XMLHttpRequest.prototype.send;
-
-  XMLHttpRequest.prototype.send = function(body) {
-    const data = JSON.parse(body);
-
-    /* Appends the domain to the username */
-    if (data && data.username) {
-    const domainInput = document.getElementById('domain-select');
-
-    if (domainInput) {
-      const atIndex = data.username.indexOf('@');
-      const usernameEndIndex = atIndex === -1 ? data.username.length : atIndex;
-
-      data.username = data.username.substring(0, usernameEndIndex) + domainInput.value;
-      }
-    }
-
-    httpSend.apply(this, arguments);
-  }
-}
+console.log('On load hook...');
 
 /* Calls functions */
 appendDomainSelect();
-updateRequestHandler();
